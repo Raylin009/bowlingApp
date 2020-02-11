c29f85dce5c122389e25c8cd9a5442ec5482725e
Ray Lin <raylin009@gmail.com>
2020-02-10 18:25:15 -0800

    Bowling Game all test pass mvp

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
-function sum(a, b) {
-  return a + b;
+
+class BowlingGame {
+  constructor(record){ 
+    this.record = record
+  }
+
+  get score (){
+    let score = 0;
+    let i = 0;
+    for(let fram = 0; fram < 10; fram += 1){
+      let curScore = 0;
+
+      if(this.record[i] === 10){
+        curScore = 10 + this.record[i + 1] + this.record[i + 2];
+        i += 1
+        //strike
+      }else if(this.record[i] + this.record[i + 1] === 10){
+        curScore = 10 + this.record[i + 2];
+        i += 2
+        //spare
+      }else{
+        curScore = this.record[i] + this.record[i + 1];
+        i += 2
+        //0-9
+      }
+      score = score + curScore;
+    }
+
+    return score
+  }
+
 }
-module.exports = sum;
+
+module.exports.BowlingGame = BowlingGame;
--- app.test.js
+++ app.test.js
@@ -1,5 +1,31 @@
-const sum = require('./app');
+const { BowlingGame } = require('./app');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('BowlingGame should exist and is a class', () => {
+  expect(new BowlingGame().constructor.name).toBe('BowlingGame');
 });
+
+test('Score should be 0 if bowl 0 in all shoots', () => {
+  const game = new BowlingGame([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+
+  expect(game.score).toBe(0)
+});
+
+test('Bowl 0-9 for every round', () => {
+  const game = new BowlingGame([2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3]);
+
+  expect(game.score).toBe(50)
+});
+
+test('spare should be sum of next round', () => {
+  const game1 = new BowlingGame([5,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+  const game2 = new BowlingGame([0,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+
+  expect(game1.score).toBe(20);
+  expect(game2.score).toBe(10);
+});
+
+test('all 10s', () => {
+  const game = new BowlingGame([10,10,10,10,10,10,10,10,10,10,10,10]);
+
+  expect(game.score).toBe(300)
+})
